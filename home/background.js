134308d492fd68f630a214da7c6bc5a6b734f7d1
1279923 <46130416+1279923@users.noreply.github.com>
2019-12-18 12:37:19 +0900

    ok

--- home/background.js
+++ home/background.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 	
-	var btn = document.querySelectorAll("button");
+	var btn = document.getElementsByClassName("navIcon");
 	for(let i=0; i<btn.length; i++){
 		btn[i].addEventListener("click", function(){
 			window.location.href = "https://1279923.github.io/recipeApp/"+String(this.id);
